b3adc171bb4f9c4af959a6a61b33da119f294075
Muhammad Muqtasid Roziq <156625812+MuqtasidRoziq@users.noreply.github.com>
2026-09-17 14:03:05 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// URL UMUM
+define('URL_DOMAIN', 'http://localhost:8080/websitesekolah.co.id/');
+define('URL_ASSETS', 'http://localhost:8080/websitesekolah.co.id/public/assets/');
+define('URL_ASSETS_BLOG', 'http://localhost:8080/cms-mysch.id/public/uploads/');
+define('URL_WHATSAPP', 'https://wa.me/6281228237219?text=Hai%20Mysch.id,%20Saya%20mau%20bertanya%20tentang%20website%20sekolah..');
+define('URL_ORDER', 'https://mysch.id/orders/?produk=website');
+
+
+// DIR APP
+define('DIR_CONTROLLERS', '/opt/lampp/htdocs/websitesekolah.co.id/app/controllers/');
+define('DIR_MODELS', '/opt/lampp/htdocs/websitesekolah.co.id/app/models/');
+define('DIR_CORE', '/opt/lampp/htdocs/websitesekolah.co.id/app/core/');
+define('DIR_ROUTES', '/opt/lampp/htdocs/websitesekolah.co.id/app/routes/');
+
+// DIR VIEW
+define('DIR_LAYOUTS', '/opt/lampp/htdocs/websitesekolah.co.id/app/views/layouts/');
+define('DIR_COMPONENTS', '/opt/lampp/htdocs/websitesekolah.co.id/app/views/layouts/components/');
+define('DIR_PAGES', '/opt/lampp/htdocs/websitesekolah.co.id/app/views/pages/');
+
+// DB CONFIG
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'website_satelit');
+define('DB_PORT', '3307');
+?>
